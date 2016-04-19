f670a9970332c683e23ae412f1920ca8ae060d51
Yashas Gujjar <Yashas Gujjar>
2016-04-18 20:34:09 -0600

    first build of nirvana, AI bird motion that slerps between 2 points done

---
+++ Assets/AIRelocate.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AIRelocate : MonoBehaviour {
+	public Transform m_othercube;
+	// Use this for initialization
+	void OnTriggerEnter(Collider i_other){
+		if (i_other.tag.Equals ("AIBird")) {
+			i_other.gameObject.GetComponent<AIBirdMotion> ().setNewDestination(m_othercube);
+		}
+
+	}
+}
