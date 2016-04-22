046b4dfdd506954b6683947f0468ffa821295002
yashasg <yashas.gujjar@gmail.com>
2016-04-22 01:06:01 -0600

    made basic scene from chloe's models.Didnt prefab them yet.Will prefab after everything works well

--- Assets/AIRelocate.cs
+++ Assets/AIRelocate.cs
@@ -6,8 +6,14 @@ public class AIRelocate : MonoBehaviour {
 	// Use this for initialization
 	void OnTriggerEnter(Collider i_other){
 		if (i_other.tag.Equals ("AIBird")) {
+            WaitinSecs(2);
 			i_other.gameObject.GetComponent<AIBirdMotion> ().setNewDestination(m_othercube);
 		}
-
+   
 	}
+    IEnumerator WaitinSecs(int i_Seconds=0)
+    {
+        yield return new WaitForSeconds(i_Seconds);
+
+    }
 }
