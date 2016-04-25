e35285bb0e4ca70380c772c78d80e2ed12316504
Yashas Gujjar <Yashas Gujjar>
2016-04-24 22:11:19 -0600

    used reticle to find moving game objects and implemented a score system

--- Assets/AIBirdMotion.cs
+++ Assets/AIBirdMotion.cs
@@ -2,7 +2,8 @@
 using System.Collections;
 using System;
 
-public class AIBirdMotion : MonoBehaviour {
+public class AIBirdMotion : MonoBehaviour, ICardboardGazeResponder
+{
 	Rigidbody m_thisRigidBody;
 	public float m_birdVelocity;
 	public Transform m_startMarker;
@@ -10,7 +11,8 @@ public class AIBirdMotion : MonoBehaviour {
 	private float startTime;
 	private float journeyLength;
     public bool isFish;
-
+    private Color m_CurrentColor=Color.white;
+    private float m_Score = 0;
 	// Use this for initialization
 	void Start () {
 		setNewDestination (m_endMarker);
@@ -22,8 +24,11 @@ public class AIBirdMotion : MonoBehaviour {
 			startTime = Time.time;
 			journeyLength = Vector3.Distance(transform.position, m_endMarker.position);
             if (!isFish)
-                transform.LookAt (m_endMarker.position,Vector3.up);
+            transform.LookAt (m_endMarker.position,Vector3.up);
 			m_thisRigidBody = GetComponent<Rigidbody> ();
+            if (GetComponent<Renderer>().material.color.Equals(m_CurrentColor))
+                m_Score += 0.5f;
+                Debug.Log(m_Score);
 
 
         }
@@ -31,14 +36,69 @@ public class AIBirdMotion : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		float distCovered = (Time.time - startTime) * m_birdVelocity;
+
+        float distCovered = (Time.time - startTime) * m_birdVelocity;
 		float fracJourney = distCovered / journeyLength;
-        if(!isFish)
-		m_thisRigidBody.position = Vector3.Slerp(transform.position, m_endMarker.position, fracJourney);
-        else
-        {
             m_thisRigidBody.position = Vector3.Lerp(transform.position, m_endMarker.position, fracJourney);
-        }
 
 	}
+    void LateUpdate()
+    {
+        Cardboard.SDK.UpdateState();
+        if (Cardboard.SDK.BackButtonPressed)
+        {
+            Application.Quit();
+        }
+    }
+    public float getScore()
+    {
+        return m_Score;
+    }
+    public void SetGazedAt(bool gazedAt)
+    {
+        System.Random rand = new System.Random();
+        m_CurrentColor = new Color((float)rand.NextDouble(), (float)rand.NextDouble(), (float)rand.NextDouble());
+        GetComponent<Renderer>().material.color = gazedAt ? m_CurrentColor : Color.yellow;
+    }
+
+ public void ToggleDistortionCorrection()
+    {
+        switch (Cardboard.SDK.DistortionCorrection)
+        {
+            case Cardboard.DistortionCorrectionMethod.Unity:
+                Cardboard.SDK.DistortionCorrection = Cardboard.DistortionCorrectionMethod.Native;
+                break;
+            case Cardboard.DistortionCorrectionMethod.Native:
+                Cardboard.SDK.DistortionCorrection = Cardboard.DistortionCorrectionMethod.None;
+                break;
+            case Cardboard.DistortionCorrectionMethod.None:
+            default:
+                Cardboard.SDK.DistortionCorrection = Cardboard.DistortionCorrectionMethod.Unity;
+                break;
+        }
+    }
+
+    #region ICardboardGazeResponder implementation
+
+    /// Called when the user is looking on a GameObject with this script,
+    /// as long as it is set to an appropriate layer (see CardboardGaze).
+    public void OnGazeEnter()
+    {
+        SetGazedAt(true);
+    }
+
+    /// Called when the user stops looking on the GameObject, after OnGazeEnter
+    /// was already called.
+    public void OnGazeExit()
+    {
+        SetGazedAt(false);
+    }
+
+    // Called when the Cardboard trigger is used, between OnGazeEnter
+    /// and OnGazeExit.
+    public void OnGazeTrigger()
+    {
+    }
+
+    #endregion
 }
---
+++ Assets/ScoreKeeper.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ScoreKeeper : MonoBehaviour {
+
+    public GameObject m_Fish;
+    public GameObject m_Bird;
+    public float m_timeLeft;
+    public float m_ScoreTime=3.0f;
+    private Text m_textField;
+    // Use this for initialization
+    void Start () {
+        m_textField = GetComponent<Text>();
+        m_Bird.SetActive(false);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        m_timeLeft -= Time.deltaTime;
+        m_textField.text = "Time Left: " + m_timeLeft;
+        if (m_timeLeft < 0.0f) {
+            if (m_Bird.activeSelf)
+            {
+                m_ScoreTime -= Time.deltaTime;
+                m_textField.text = "X Score: " + m_Bird.GetComponent<AIBirdMotion>().getScore();
+                if (m_ScoreTime <= 0.0f)
+                {
+                    m_ScoreTime = 3.0f;
+                    m_Bird.SetActive(false);
+                    m_Fish.SetActive(true);
+                    m_timeLeft = 10.0f;
+                }
+            }
+            else
+            {
+                m_ScoreTime -= Time.deltaTime;
+                m_textField.text = "Y Score: " + m_Fish.GetComponent<AIBirdMotion>().getScore();
+                if (m_ScoreTime <= 0.0f)
+                {
+                    m_ScoreTime = 3.0f;
+                    m_Fish.SetActive(false);
+                    m_Bird.SetActive(true);
+                    m_timeLeft = 10.0f;
+                }
+            }
+                              }
+
+    }
+}
