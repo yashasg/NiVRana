a68347620c698375f09a09c1900026cdd9112c7c
Yashas Gujjar <Yashas Gujjar>
2016-04-24 22:11:19 -0600

    used reticle to find moving game objects and implemented a score system

--- Assets/AIRelocate.cs
+++ Assets/AIRelocate.cs
@@ -5,9 +5,14 @@ public class AIRelocate : MonoBehaviour {
 	public Transform m_othercube;
 	// Use this for initialization
 	void OnTriggerEnter(Collider i_other){
+
 		if (i_other.tag.Equals ("AIBird")) {
 			i_other.gameObject.GetComponent<AIBirdMotion> ().setNewDestination(m_othercube);
 		}
+        else
+        {
+            i_other.gameObject.GetComponent<AIBirdMotion>().setNewDestination(m_othercube);
+        }
    
 	}
 
