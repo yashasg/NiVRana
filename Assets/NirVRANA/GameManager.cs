06420932df7718664dd323b63016930695e4e387
u1006341 <gujjaryashas@gmail.com>
2017-03-26 18:15:04 -0600

    game states will toggle the rock on and off.

--- Assets/NirVRANA/GameManager.cs
+++ Assets/NirVRANA/GameManager.cs
@@ -3,10 +3,60 @@ using System.Collections;
 
 public class GameManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
+    public GameObject[] children;
+
+    enum eGameStates
+    {
+        MainMenu=0,
+        BeginRound=1,
+        ShowScore=2,
+        ShowFinalScore=3
+    };
+    private eGameStates currentGameState;
+
+
+    void Start()
+    {
+        currentGameState = eGameStates.MainMenu;
+    }
+
+   public void MoveToNextState()
+    {
+        switch (currentGameState)
+        {
+            case eGameStates.MainMenu:
+                currentGameState = eGameStates.BeginRound;
+                ToggleChildren(false);
+                break;
+            case eGameStates.BeginRound:
+                currentGameState = eGameStates.ShowScore;
+                ToggleChildren(true);
+                break;
+            case eGameStates.ShowScore:
+                currentGameState = eGameStates.BeginRound;
+                ToggleChildren(false);
+                break;
+        }
+    }
+
+    public void MoveToFinalScore()
+    {
+        ToggleChildren(true);
+        //show suwas' score here.
+
+
+    }
+
+
+    private void ToggleChildren(bool isEnabled)
+    {
+        foreach(GameObject child in children)
+        {
+            child.SetActive(isEnabled);
+        }
+    }
+
+
 	
 	// Update is called once per frame
 	void Update () {
